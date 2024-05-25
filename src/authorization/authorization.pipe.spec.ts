0147ab399154c221b93c9ee0288872e765431243
sgh4123@naver.com <sgh4123@naver.com>
2024-05-25 21:46:56 +0900

    feat: add pipe for session login
    -세션 로그인 기능을 구현하기 위한 pipe 선언

---
+++ src/authorization/authorization.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { AuthorizationPipe } from './authorization.pipe';
+
+describe('AuthorizationPipe', () => {
+  it('should be defined', () => {
+    expect(new AuthorizationPipe()).toBeDefined();
+  });
+});
