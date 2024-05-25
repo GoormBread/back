339585d4ec76031dbc37983920f3bae195fd8cc6
sgh4123@naver.com <sgh4123@naver.com>
2024-05-25 21:07:10 +0900

    chore: add dependecy bcrypt for auth service
    -로그인 서비스 구축을 위한 bcrypt 디펜던시 추가\n-auth 컨트롤러에 API 명세서에 맞게 컨트롤러 구축

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+
+    //Pipe 사용
+    @Post('/register')
+    async registerUser(@Body() ){
+        
+    }
+
+    //Pipe 사용
+    @Post('/login')
+    async login(@Body() ){
+
+    }
+
+    //Cookie의 Seesion ID 사용
+    @Post('/logout')
+    async logout(){
+
+    }
+
+}
