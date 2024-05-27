a0292e335b7d186dec89365de1aee8da20e7a52e
sgh4123@naver.com <sgh4123@naver.com>
2024-05-27 22:24:40 +0900

    chore: add dtos and logic about lobby function
    -로비 기능을 구현하기 위한 dto 추가
    -로비 기능을 구현하기 위한 컨트롤러 매핑 및 서비스 함수 추가(비어 있음)

--- src/lobby/lobby.service.ts
+++ src/lobby/lobby.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import Redis from 'ioredis';
+import { LOBBY_REDIS } from 'src/redis/redis.constants';
+import { LobbyDto } from './dto/in/Lobby.dto';
+import { PatchLobbyDto } from './dto/in/PatchLobby.dto';
 
 @Injectable()
-export class LobbyService {}
+export class LobbyService {
+    constructor(@Inject(LOBBY_REDIS) private readonly redis: Redis){}
+    async getLobbyInformation(lobby_id: string) {
+        throw new Error('Method not implemented.');
+    }
+    async createLobby(lobbyDto: LobbyDto) {
+        throw new Error('Method not implemented.');
+    }
+    async removeLobby(lobby_id: string) {
+        throw new Error('Method not implemented.');
+    }
+    async patchLobby(patchLobbyDto: PatchLobbyDto) {
+        throw new Error('Method not implemented.');
+    }
+    async getAllLobbyInformation() {
+        throw new Error('Method not implemented.');
+    }   
+}
