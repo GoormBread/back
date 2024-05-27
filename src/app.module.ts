414973b51f0284783278f13a865a575019a25feb
sgh4123@naver.com <sgh4123@naver.com>
2024-05-27 21:46:08 +0900

    chore: add files and folders about lobby function
    -로비 기능을 구축하기 위한 관련 폴더 및 파일들 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Global, Inject, MiddlewareConsumer, Module, NestModule } from '@nestjs/
 import { AuthModule } from './auth/auth.module';
 import { PrismaClient } from '@prisma/client';
 import { RedisModule } from './redis/redis.module';
+import { LobbyModule } from './lobby/lobby.module';
 
 @Global()
 @Module({
   imports: [
     AuthModule,
     RedisModule,
+    LobbyModule,
   ],
   controllers: [],
   providers: [PrismaClient],
