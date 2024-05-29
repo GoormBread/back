7694369ffca787b873f48dbbdd4e18d67eabda84
sgh4123@naver.com <sgh4123@naver.com>
2024-05-29 18:42:39 +0900

    config: add user module, controller, service
    -유저 API 제작을 위한 moudule, controller, service 선언

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaClient } from '@prisma/client';
 import { RedisModule } from './redis/redis.module';
 import { LobbyModule } from './lobby/lobby.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
 
 @Global()
 @Module({
@@ -10,9 +14,10 @@ import { LobbyModule } from './lobby/lobby.module';
     AuthModule,
     RedisModule,
     LobbyModule,
+    UserModule,
   ],
-  controllers: [],
-  providers: [PrismaClient],
+  controllers: [UserController],
+  providers: [PrismaClient, UserService],
   exports: [PrismaClient],
 })
 export class AppModule{
