d17649eaa56b97f1f622cc9853f9eea66dd16d47
sgh4123@naver.com <sgh4123@naver.com>
2024-06-06 17:21:02 +0900

    chore: init controller, service function
    -game api에 사용할 controller, service에 함수 선언

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { GameService } from './game.service';
+import { throwErrorHttp } from 'src/error';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+    constructor(private readonly gameService: GameService){}
+
+    @Get('/all')
+    async getAllGameList(){
+        try{
+            return await this.gameService.getAllGameInformation();
+        }
+        catch(error){
+            throwErrorHttp(error);
+        }
+    }
+
+    @Get('/:gameId')
+    async getGameInformtaion(@Param('gameId') gameId: string){
+        try{
+            return await this.gameService.getGameInfo(gameId);
+        }
+        catch(error){
+            throwErrorHttp(error);
+        }
+    }
+
+    @Get('/:gameId/pad')
+    async getGameCommand(@Param('gameId') gameId: string){
+        try{
+            return await this.gameService.getGameCommand(gameId);
+        }
+        catch(error){
+            throwErrorHttp(error);
+        }
+    }
+}
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+    constructor(private readonly prisma: PrismaClient){}
+    async getGameCommand(gameId: string) {
+        throw new Error('Method not implemented.');
+    }
+    async getGameInfo(gameId: string) {
+        throw new Error('Method not implemented.');
+    }
+    async getAllGameInformation() {
+        throw new Error('Method not implemented.');
+    }
+}
