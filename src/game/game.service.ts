1d1427b8fcc79d7936d7d10b01f00dcc390075f2
sgh4123@naver.com <sgh4123@naver.com>
2024-06-06 17:31:02 +0900

    feat: add game api endpoint
    -게임 관련 API에 대한 서비스 로직 추가 완료
    -게임 관련 API 제작 완료

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,16 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { MESSAGES } from '@nestjs/core/constants';
 import { PrismaClient } from '@prisma/client';
+import { STATUS_CODES } from 'http';
 
 @Injectable()
 export class GameService {
-    constructor(private readonly prisma: PrismaClient){}
+    constructor(private readonly prisma: PrismaClient) { }
     async getGameCommand(gameId: string) {
-        throw new Error('Method not implemented.');
+        const gameCommand = await this.prisma.game.findUnique({
+            select: {
+                game_command: true
+            },
+            where: {
+                game_id: gameId,
+            },
+        });
+        if (gameCommand !== null) {
+            return {
+                MESSAGE: 'Get GameCommand Success!',
+                STATUS_CODES: 200,
+                gameCommand,
+            };
+        }
+        else {
+            throw new NotFoundException();
+        }
     }
+
     async getGameInfo(gameId: string) {
-        throw new Error('Method not implemented.');
+        const gameInfo = await this.prisma.game.findUnique({
+            where: {
+                game_id: gameId,
+            },
+        });
+        if (gameInfo !== null) {
+            return {
+                MESSAGE: 'Get GameInfo Success!',
+                STATUS_CODES: 200,
+                gameInfo,
+            };
+        }
+        else {
+            throw new NotFoundException();
+        }
     }
+
     async getAllGameInformation() {
-        throw new Error('Method not implemented.');
+        const allGameInfo = await this.prisma.game.findMany();
+        return {
+            MESSAGE: 'Get AllGameInfo Success!',
+            STATUS_CODES: 200,
+            allGameInfo,
+        };
     }
 }
