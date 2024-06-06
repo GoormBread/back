6e0d325c58a6467864ee97ae71c4e27ded545029
sgh4123@naver.com <sgh4123@naver.com>
2024-06-06 20:21:54 +0900

    docs: add swagger-ui base Server url
    - api를 호출할 때 기본 서버 경로 설정

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
                                       .setDescription("구름빵 API입니다.")
                                       .setVersion('1.0')
                                       .addTag('goormbread')
+                                      .addServer('http://paran2024.iptime.org/backend')
                                       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
