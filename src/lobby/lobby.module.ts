199c4acd7bee41ac599a110e0ffce08c91383c93
sgh4123@naver.com <sgh4123@naver.com>
2024-06-07 06:35:44 +0900

    feat: add lobby socket io api
    -로비 시스템 socket.io를 통한 구축1

--- src/lobby/lobby.module.ts
+++ src/lobby/lobby.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LobbyService } from './lobby.service';
 import { LobbyGateway } from './lobby.gateway';
+import { RedisModule } from 'nestjs-redis';
 
 @Module({
+  imports: [RedisModule],
   controllers: [],
   providers: [LobbyService, LobbyGateway],
 })
